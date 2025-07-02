d74595ba4327498d9157c01ebd9feba2debaa313
jbelkerf <jbelkerf@gmail.com>
2025-07-02 12:51:42 +0100

    sync

--- interception_communication/udp_conection2.py
+++ interception_communication/udp_conection2.py
@@ -4,6 +4,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 add = ("10.0.0.2", 31337)
 msg = b"Hello, World!\n"
+sock.bind(("10.0.0.1", 31338))
 sock.sendto(msg, add)
 
 print(sock.recvfrom(1024))
