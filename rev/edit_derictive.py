3ef5f4677be1e56b4ea1dad5cb7f7a4b4a89fffa
jbelkerf <jbelkerf@gmail.com>
2025-10-12 15:47:49 +0100

    sync from pers

--- rev/edit_derictive.py
+++ rev/edit_derictive.py
@@ -10,15 +10,7 @@ x = 0
 print('starting ...')
 while (i < 71698):
     byte1 = f.read(1)
-    if byte1 == b'\x06':
-        print('found derictive 6')
-        byte2 = f.read(1)
-        byte3 = f.read(1)
-        if byte2 == b'\x00' and byte3 == b'\x01':
-            t.write(b'\x06\x00\x00')
-        else:
-            t.write(byte1)
-    elif byte1 == b'\x07':
+    if byte1 == b'\x07':
         print('found derictive 7')
         byte2 = f.read(1)
         byte3 = f.read(1)
@@ -27,7 +19,11 @@ while (i < 71698):
         if byte2 == b'\x00' and byte3 == b'\x93' and byte4 == b'\x30' and byte5 == b'\x0b':
             t.write(b'\x07\x00\x00\x00\x00')
         else:
-            t.write(b'\x07')
+            t.write(byte1)
+            t.write(byte2)
+            t.write(byte3)
+            t.write(byte4)
+            t.write(byte5)
     else:
         t.write(byte1)
     i+=1
