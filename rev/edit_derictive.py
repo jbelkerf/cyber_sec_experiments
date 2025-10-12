282e388157b675d88cfcc385549207bd4fdcd598
jbelkerf <jbelkerf@gmail.com>
2025-10-12 11:38:55 +0100

    sync

--- rev/edit_derictive.py
+++ rev/edit_derictive.py
@@ -1,17 +1,20 @@
 
-f = open("flag.cimg", 'rb')
-t = open("to.cimg", 'wb')
+f = open("/challenge/flag.cimg", 'rb')
+t = open("flag.cimg", 'wb')
 # pwn.college{MBLzkB3d3VQWF72lxHCFAJPa22A.QXyADN4EDL4IzN4czW}
 i = 0
-while (i < 0x4d * 0x31):
+x = 0
+# byte1 = f.read(1)
+# t.write(byte1)
+while (i < 71699):
     byte1 = f.read(1)
-    byte2 = f.read(1)
-    print(f"b1 == {byte1}   b2 == {byte2}")
-    if byte1 == b'\x07' and byte2 == b'\x00':
-        print("haaaa")
-        t.write((b"\x06"))
-        t.write((b"\x00"))
+    if byte1 == '\x06':
+        if f.read() == '\x00' and f.read() == '\x01':
+            t.write(b'\x06\x00\x00')
+        else:
+            t.write(byte1)
+    elif byte1 == '\x07':
+        pass
     else:
         t.write(byte1)
-        t.write(byte2)
-    i+=2
+    i+=1
